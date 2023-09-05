5ad043636bbc9ab0e9c0da9ab20ad43ae781faac
Majid Zahedi <emajidzahedi@gmail.com>
2023-09-05 08:53:20 +0330

    language switcher + metadata
    i've never used description on commits i don't know how it looks!

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -6,8 +6,10 @@ export default function Index() {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-y-5 bg-gradient-to-tr from-blue-200 to-purple-200 text-gray-900">
       <h1 className="text-3xl font-bold">{t('title')}</h1>
-      <p className="max-w-2xl text-justify">{t('description')}</p>
-      <LanguageSwitcher />
+      <p className="max-w-2xl">{t('description')}</p>
+      <div className="fixed right-5 top-5">
+        <LanguageSwitcher />
+      </div>
     </div>
   )
 }
--- components/language-switcher.tsx
+++ components/language-switcher.tsx
@@ -1,17 +1,21 @@
 'use client'
-import Link from 'next-intl/link'
 
 import { useLocale } from 'next-intl'
+import { Languages } from 'lucide-react'
+import { usePathname } from 'next-intl/client'
+import Link from 'next-intl/link'
 
-import { usePathname } from 'next/navigation'
+import { Button } from '@/components/ui/button'
 
-export const LanguageSwitcher = (props: {}) => {
+export const LanguageSwitcher = () => {
   const pathname = usePathname()
   const locale = useLocale()
 
   return (
     <Link href={pathname} locale={locale === 'fa' ? 'en' : 'fa'}>
-      LanguageSwitcher
+      <Button size="icon" variant="link">
+        <Languages />
+      </Button>
     </Link>
   )
 }
